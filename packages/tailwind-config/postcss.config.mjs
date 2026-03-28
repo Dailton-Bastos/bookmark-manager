18efe2aee007be9146a7f0a587226b26b7520368
Dailton Bastos <dailtonbastos@gmail.com>
2026-03-28 15:34:32 -0300

    fix(ui): imports and postcss.config

--- packages/tailwind-config/postcss.config.mjs
+++ packages/tailwind-config/postcss.config.mjs
@@ -1,13 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 
-import path from 'node:path';
-
-const baseDir = path.resolve(new URL('.', import.meta.url).pathname, '..', '..');
-
 export default {
 	plugins: {
-		'@tailwindcss/postcss': {
-			base: baseDir
-		}
+		'@tailwindcss/postcss': {}
 	}
 }
