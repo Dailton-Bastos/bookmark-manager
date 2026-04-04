3a989b77bb155bfb358a6f7f4c70a93c0d6bad59
Dailton Bastos <dailtonbastos@gmail.com>
2026-04-04 19:07:57 -0300

    feat(web): implement toast sonner

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,14 @@
+'use client'
+
+import { toast } from 'sonner'
 import { Button } from 'ui/components/shadcn/ui/button'
 
 export default function Home() {
 	return (
 		<div className="flex flex-col items-center justify-center h-screen">
-			<Button>Test</Button>
+			<Button onClick={() => toast.success('Bookmark added successfully.')}>
+				Test
+			</Button>
 		</div>
 	)
 }
