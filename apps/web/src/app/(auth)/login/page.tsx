16df228df9c98f263346f35c0d2dae46aa059b39
Dailton Bastos <dailtonbastos@gmail.com>
2026-04-04 21:44:19 -0300

    feat(web): implement toast notifcations for login and promises

--- apps/web/src/app/(auth)/login/page.tsx
+++ apps/web/src/app/(auth)/login/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import type { LoginFormData } from '@repo/schemas'
-import { useSearchParams } from 'next/navigation'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { toast } from 'sonner'
 import { LoginForm } from '@/components/auth/login-form'
 import { authClient } from '@/lib/auth-client'
 import { DEFAULT_LOGIN_REDIRECT } from '@/routes'
@@ -25,19 +27,49 @@ function getSafeCallbackUrl(callbackUrl: string | null): string {
 export default function LoginPage() {
 	const searchParams = useSearchParams()
 
+	const queryClient = useQueryClient()
+
+	const router = useRouter()
+
 	const callbackUrl = searchParams.get('callbackUrl')
 
-	const handleLogin = async (data: LoginFormData): Promise<void> => {
-		await authClient.signIn.email({
-			email: data.email,
-			password: data.password,
-			callbackURL: getSafeCallbackUrl(callbackUrl)
+	const { mutateAsync, isPending } = useMutation({
+		mutationFn: async (formData: LoginFormData) => {
+			await authClient.signIn.email(
+				{
+					email: formData.email,
+					password: formData.password
+				},
+				{
+					onSuccess: (ctx) => {
+						if (!ctx.data?.user) return
+
+						queryClient.setQueryData(['current-user'], ctx.data.user)
+					},
+					onError: (ctx) => {
+						throw new Error(ctx.error.message)
+					}
+				}
+			)
+		},
+		onSuccess: () => router.push(getSafeCallbackUrl(callbackUrl))
+	})
+
+	const onSubmit = async (data: LoginFormData) => {
+		toast.promise(mutateAsync(data), {
+			loading: 'Logging in...',
+			success: 'Logged in successfully!',
+			error: (err) => {
+				if (err instanceof Error) return err.message
+
+				return 'An error occurred while logging in.'
+			}
 		})
 	}
 
 	return (
 		<div className="max-w-md w-full space-y-8">
-			<LoginForm onSubmit={handleLogin} />
+			<LoginForm onSubmit={onSubmit} isPending={isPending} />
 		</div>
 	)
 }
