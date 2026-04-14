b9044749ba7d6a64ebd26a2caac72dabca1314d6
Dailton Bastos <dailtonbastos@gmail.com>
2026-04-13 21:48:34 -0300

    feat(bookmarks): update create bookmark method to include ownerId from session

--- apps/api/src/bookmarks/bookmarks.controller.ts
+++ apps/api/src/bookmarks/bookmarks.controller.ts
@@ -1,6 +1,7 @@
 import { Controller } from '@nestjs/common'
 import { Implement, implement, ORPCError } from '@orpc/nest'
 import { contract } from '@repo/contract'
+import { Session, type UserSession } from '@thallesp/nestjs-better-auth'
 import { BookmarksService } from './bookmarks.service'
 
 @Controller()
@@ -8,9 +9,12 @@ export class BookmarksController {
 	constructor(private readonly bookmarksService: BookmarksService) {}
 
 	@Implement(contract.bookmark.create)
-	create() {
+	create(@Session() session: UserSession) {
 		return implement(contract.bookmark.create).handler(async ({ input }) => {
-			const bookmark = await this.bookmarksService.create(input)
+			const bookmark = await this.bookmarksService.create(
+				input,
+				session.user.id
+			)
 
 			if (!bookmark) {
 				throw new ORPCError('INTERNAL_SERVER_ERROR', {
