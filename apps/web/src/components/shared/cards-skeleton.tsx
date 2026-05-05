43ad52d6b83cf3e4885a65c89e3be9cfcd3d918c
Dailton Bastos <dailtonbastos@gmail.com>
2026-05-04 22:43:12 -0300

    feat(web): add loading skeleton component for bookmarks list

---
+++ apps/web/src/components/shared/cards-skeleton.tsx
@@ -0,0 +1,18 @@
+import { uuid } from '@/utils/uuid'
+
+export const CardsSkeleton = () => {
+	return (
+		<>
+			{Array.from({ length: 8 }).map((_) => (
+				<div
+					key={`skeleton-${uuid()}`}
+					className="animate-pulse flex flex-col gap-2"
+				>
+					<div className="relative flex flex-col text-muted-foreground bg-clip-border rounded-xl animate-pulse">
+						<div className="relative grid min-h-68 overflow-hidden text-gray-700 bg-gray-300 bg-clip-border rounded-xl place-items-center" />
+					</div>
+				</div>
+			))}
+		</>
+	)
+}
