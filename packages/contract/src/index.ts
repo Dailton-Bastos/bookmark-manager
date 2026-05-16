abf48355dedee97fcf87c11bad223f8819ba26d7
Dailton Bastos <dailtonbastos@gmail.com>
2026-05-16 09:16:03 -0300

    feat(contract): add pin/unpin bookmark functionality to the API

--- packages/contract/src/index.ts
+++ packages/contract/src/index.ts
@@ -3,13 +3,15 @@ import { populateContractRouterPaths } from '@orpc/contract'
 import {
 	archivedUnarchivedBookmarkContract,
 	createBookmarkContract,
-	listBookmarksContract
+	listBookmarksContract,
+	pinUnpinBookmarkContract
 } from './bookmark'
 
 export const contract = populateContractRouterPaths({
 	bookmark: {
 		create: createBookmarkContract,
 		list: listBookmarksContract,
-		archiveOrUnarchive: archivedUnarchivedBookmarkContract
+		archiveOrUnarchive: archivedUnarchivedBookmarkContract,
+		pinOrUnpin: pinUnpinBookmarkContract
 	}
 })
